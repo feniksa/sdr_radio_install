4bfd2da0016ccbbae4397e4334418ff65da2b7fd
Maksym Sditanov <msditanov@200volts.com>
2022-05-06 00:01:09 +0300

    Add script for installation for Ubuntu 20
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Maksym Sditanov <msditanov@200volts.com>

---
+++ install_openwebrx.bash
@@ -0,0 +1,235 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_PATH=`realpath "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+BUILD_DIR="$SCRIPT_DIR/build"
+
+function install_prerequisites()
+{
+	sudo apt-get install git build-essential cmake python3 python3-setuptools 
+	sudo apt-get install rtl-sdr netcat libsndfile-dev librtlsdr-dev automake autoconf libtool pkg-config fftw3-dev
+}
+
+function install_cdr() 
+{
+	echo "Install csdr"
+
+	cd "$BUILD_DIR"
+
+	[[ ! -d csdr ]] && git clone --depth=1 -b master https://github.com/jketterl/csdr.git
+	cd csdr
+	autoreconf -i
+	./configure
+	make
+	sudo make install
+	cd ..
+	sudo ldconfig
+}
+
+function install_js8py()
+{
+	echo "Install js8py library from source"
+	cd "$BUILD_DIR"
+	[[ ! -d js8py ]] && git clone --depth=1 -b master https://github.com/jketterl/js8py.git
+	cd js8py
+	sudo python3 setup.py install
+	cd ..
+}
+
+function install_owrx_connector()
+{
+	echo "Install owrx_connector from source"
+
+	cd "$BUILD_DIR"
+
+	[[  ! -d owrx_connector ]] && git clone --depth=1 -b master https://github.com/jketterl/owrx_connector.git
+
+	cd owrx_connector
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ../..
+	sudo ldconfig
+}
+
+function install_digital_voice()
+{
+	echo "Install codecserver (for digital voice)"
+
+	sudo apt-get install sox libprotobuf-dev protobuf-compiler
+
+	cd "$BUILD_DIR"
+	[[ ! -d codecserver ]] && git clone --depth=1 -b master https://github.com/jketterl/codecserver.git
+	cd codecserver
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ../..
+	sudo ldconfig
+}
+
+function setup_codecserver_users()
+{
+	echo "Create codecserver user and set permissions for serial devices"
+	sudo bash -c "id -u codecserver >/dev/null 2>&1 || adduser --system --group --no-create-home --home /nonexistent --quiet codecserver"
+	sudo bash -c "id -u codecserver >/dev/null 2>&1 || usermod -aG dialout codecserver"
+}
+
+
+function install_digiham()
+{
+	echo "Install digiham from source"
+	cd "$BUILD_DIR"
+	[[ ! -d digiham ]] && git clone --depth=1 -b master https://github.com/jketterl/digiham.git
+	cd digiham
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ../..
+}
+
+function install_codec2()
+{
+	echo "Install codec2 from source"
+
+	cd "$BUILD_DIR"
+
+	[[ ! -d codec2 ]] &&  git clone --depth=1 https://github.com/drowe67/codec2.git
+
+	cd codec2
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	# manually install freedv_rx since it's not part of the default install package
+	sudo install -m 0755 src/freedv_rx /usr/local/bin
+	cd ../..
+	sudo ldconfig
+}
+
+function install_m17-cxx-demod()
+{
+	echo "m17-cxx-demod"
+	sudo apt-get install libboost-program-options-dev
+	cd "$BUILD_DIR"
+	[[ ! -d m17-cxx-demod ]] && git clone --depth=1 https://github.com/mobilinkd/m17-cxx-demod.git
+	cd m17-cxx-demod
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ../..
+}
+
+
+function install_drm()
+{
+	echo "Install optional dependencies for DRM"
+
+	sudo apt-get install qt5-default libpulse0 libfaad2 libopus0 libpulse-dev libfaad-dev libopus-dev libfftw3-dev wget
+
+	cd "$BUILD_DIR"
+	[[ ! -f $BUILD_DIR/dream-2.1.1-svn808.tar.gz ]] && wget https://downloads.sourceforge.net/project/drm/dream/2.1.1/dream-2.1.1-svn808.tar.gz
+
+	[[ ! -d "$BUILD_DIR/dream" ]] && tar xvfz dream-2.1.1-svn808.tar.gz
+	cd dream
+	qmake CONFIG+=console
+	make
+	sudo make install
+	cd ..
+}
+
+function install_direwolf()
+{
+	echo "Install optional packages for Packet / APRS"
+	sudo apt-get install direwolf
+}
+
+function install_sox()
+{
+	echo "Install sox"
+	sudo apt-get install sox
+}
+
+
+function setup_openwebrx()
+{
+	echo "Prepare data storage"
+	sudo mkdir -p /var/lib/openwebrx
+
+	echo "Craete openwebrx user"
+	sudo bash -c "id -u openwebrx || useradd -m openwebrx "
+
+	echo "Change /var/lib/openwebrx owner to openwebrx:openwebrx"
+	sudo chown openwebrx:openwebrx /var/lib/openwebrx
+
+	echo "Create empty users.json"
+	sudo bash -c "echo [] > /var/lib/openwebrx/users.json"
+
+	echo "Change owner/group for users.json to openwebrx:openwebrx"
+	sudo chown openwebrx:openwebrx /var/lib/openwebrx/users.json
+
+	echo "users.json 0600"
+	sudo chmod 0600 /var/lib/openwebrx/users.json
+}
+
+
+function install_openwebrx()
+{
+	echo "Clone openwebrx"
+	cd "$BUILD_DIR"
+
+	[[ ! -d openwebrx ]] && git clone --depth=1 -b master https://github.com/jketterl/openwebrx.git
+}
+
+function post_setup_openwebrx()
+{
+	echo "Create admin user for  openwebrx"
+	cd openwebrx
+	sudo ./openwebrx.py admin adduser admin
+	#./openwebrx.py
+
+	echo "Create openwebrx service file"
+	sudo bash -c "echo '[Unit]
+Description=OpenWebRX WebSDR receiver
+
+[Service]
+Type=simple
+User=openwebrx
+Group=openwebrx
+ExecStart=$BUILD_DIR/openwebrx/openwebrx.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/openwebrx.service"
+
+	sudo systemctl daemon-reload
+}
+
+# ------------------------------------------------------------------------
+install_prerequisites
+install_cdr
+install_js8py
+install_owrx_connector
+install_digital_voice
+setup_codecserver_users
+install_digiham
+install_codec2
+install_m17
+install_drm
+install_direwolf
+install_sox
+setup_openwebrx
+install_openwebrx
+post_setup_openwebrx
+
---
+++ install_soapy_and_modules.bash
@@ -0,0 +1,223 @@
+#! /bin/bash
+
+set -e
+
+SCRIPT_PATH=`realpath "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+BUILD_DIR="$SCRIPT_DIR/build"
+
+
+function install_prerequisites() 
+{
+	echo "Install prerequisites"
+
+	sudo apt-get install git build-essential automake cmake g++ swig
+	sudo apt-get install libgtk2.0-dev libpulse-dev python-numpy
+	sudo apt-get install mesa-utils 
+	sudo apt-get install freeglut3-dev freeglut3 gtk+-3.0
+}
+
+function install_hackrf_prerequisites() 
+{
+	if [[ ! -f /etc/apt/sources.list.d/myriadrf-ubuntu-drivers-focal.list ]]; then
+		sudo add-apt-repository -y ppa:myriadrf/drivers
+		sudo apt-get update
+	fi
+	sudo apt-get install hackrf libhackrf-dev
+}
+
+function build_liqued_dsp() 
+{
+	echo "Build liquid-dsp"
+	
+	cd "$BUILD_DIR"
+	if [ ! -d "$BUILD_DIR/liquid-dsp" ]; then
+		git clone https://github.com/jgaeddert/liquid-dsp --depth=1
+	fi
+
+	cd liquid-dsp
+	if [ ! -f /usr/local/lib/libliquid.so]; then
+		./bootstrap.sh
+		./configure --enable-fftoverride 
+		make
+		sudo make install
+		sudo ldconfig
+	fi
+	cd ..
+}
+
+function build_wxwidgets() {
+	echo "Build wxwidgets"
+
+	cd "$BUILD_DIR"
+	if [ ! -f "$BUILD_DIR/wxWidgets-3.1.1.tar.bz2" ]; then
+		wget -c https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.1/wxWidgets-3.1.1.tar.bz2
+	fi
+
+	if [ ! -d "$BUILD_DIR/wxWidgets-3.1.1" ]; then
+		tar -xvjf wxWidgets-3.1.1.tar.bz2  
+	fi
+
+	cd wxWidgets-3.1.1
+	if [ -f $BUILD_DIR/wxWidgets-staticlib/bin/wx-config ]; then
+		return 0
+	fi
+
+	mkdir -p "$BUILD_DIR/wxWidgets-staticlib"
+	./autogen.sh 
+	./configure --with-opengl --disable-shared --enable-monolithic --with-libjpeg --with-libtiff --with-libpng --with-zlib --disable-sdltest --enable-unicode --enable-display --enable-propgrid --disable-webkit --disable-webview --disable-webviewwebkit --with-gtk=3 --prefix="$BUILD_DIR/wxWidgets-staticlib" CXXFLAGS="-std=c++0x" --with-libiconv=/usr
+	make
+	sudo make install
+	cd ..
+}
+
+function soap_sdr_play_service() 
+{
+	echo "[Unit]
+Description=SDRplay API Service
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=on-failure
+RestartSec=1
+User=root
+ExecStart=/usr/local/bin/sdrplay_apiService
+
+[Install]
+WantedBy=multi-user.target
+" > /tmp/sdrplay.service 
+	sudo mv /tmp/sdrplay.service /etc/systemd/system/sdrplay.service
+	sudo chown root:root /etc/systemd/system/sdrplay.service
+	sudo chmod ug+x /etc/systemd/system/sdrplay.service
+}
+
+function build_soapy_sdr() 
+{
+	echo "Build SoapySDR"
+
+	cd "$BUILD_DIR"
+
+	if [ ! -d "$BUILD_DIR/SoapySDR" ]; then
+		git clone https://github.com/pothosware/SoapySDR.git --depth=1
+	fi
+
+	cd SoapySDR
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	sudo ldconfig
+	cd ..
+	cd ..
+}
+
+# Now we build the SDRPlay module for Soapy
+function build_soapy_sdr_play() {
+	echo "Build SoapySDRPlay3"
+
+	cd "$BUILD_DIR"
+	if [ ! -d "$BUILD_DIR/SoapySDRPlay3" ]; then
+		git clone https://github.com/pothosware/SoapySDRPlay3.git --depth=1
+	fi
+
+	cd SoapySDRPlay3
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ..
+	cd ..
+}
+
+# Now we build the SDRPlay module for Soapy
+function build_soapy_hackrf() {
+	echo "Build HackRF"
+
+	cd "$BUILD_DIR"
+	if [ ! -d "$BUILD_DIR/SoapyHackRF" ]; then
+		git clone https://github.com/pothosware/SoapyHackRF.git --depth=1
+	fi
+
+	cd SoapyHackRF
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ..
+	cd ..
+}
+
+# And we build SoapyRemote
+
+function build_soapy_remote() {
+	echo "Build SoapyRemote"
+
+	cd "$BUILD_DIR"
+	if [ ! -d "$BUILD_DIR/SoapyRemote" ]; then
+		git clone https://github.com/pothosware/SoapyRemote.git --depth=1
+	fi
+
+	cd SoapyRemote
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=release ..
+	make
+	sudo make install
+	cd ..
+	cd ..
+}
+
+# And finally,  we build Cubic.  This takes awhile!
+function build_cubic_sdr() {
+	echo "Build CubicSDR"
+
+	cd "$BUILD_DIR"
+	if [ ! -d "$BUILD_DIR/CubicSDR" ]; then
+		git clone https://github.com/cjcliffe/CubicSDR.git --depth=1
+	fi
+	cd CubicSDR
+	mkdir -p build
+	cd build
+	cmake ../ -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIG_EXECUTABLE="$BUILD_DIR/wxWidgets-staticlib/bin/wx-config" -DOpenGL_GL_PREFERENCE="LEGACY"
+	make
+	sudo make install
+	sudo ldconfig
+	cd ..
+	cd ..
+}
+
+function blacklist_modules() 
+{
+	echo "Blacklist sdr unsupported kernel modules"
+	grep 'msi001' /etc/modprobe.d/blacklist.conf || sudo bash -c "echo 'blacklist sdr_msi3101
+blacklist msi001
+blacklist msi2500' >> /etc/modprobe.d/blacklist.conf"
+
+}
+
+# now we change permissions on these root-owned folders so the user can 
+# delete them at their leisure.
+
+mkdir -p "$BUILD_DIR"
+
+echo
+
+install_prerequisites
+install_hackrf_prerequisites
+build_liqued_dsp
+build_wxwidgets
+build_soapy_sdr
+build_soapy_sdr_play
+build_soapy_hackrf
+soap_sdr_play_service
+blacklist_modules
+build_soapy_remote
+build_cubic_sdr
+
+echo 'SoapySDRUtil --probe="driver=sdrplay"'
+echo 'SoapySDRUtil --probe="driver=hackrf"'
